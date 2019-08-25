b08c902238311532e5035015a476a3cd942d9bee
Undervane <sergio@mipigu.com>
2019-08-25 14:45:34 +0200

    [dev] Add HTTPS support

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { INestApplication } from '@nestjs/common';
+import * as path from 'path';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+
+  let app: INestApplication;
+
+  if (process.env.NODE_ENV === 'production') {
+
+    const key = fs.readFileSync(path.resolve(__dirname, '../ssl/privkey.pem'));
+    const cert = fs.readFileSync(path.resolve(__dirname, '../ssl/cert.pem'));
+
+    app = await NestFactory.create(AppModule, {
+      httpsOptions: {
+        key,
+        cert,
+      },
+    });
+  } else {
+    app = await NestFactory.create(AppModule);
+  }
+
   app.enableCors();
   await app.listen(process.env.APP_PORT || 3000, process.env.APP_HOST || '0.0.0.0');
 }
