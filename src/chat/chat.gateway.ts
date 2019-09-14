42015a81c91fde77f5f73dd67d504a01dfc2ec87
Undervane <sergio@mipigu.com>
2019-09-14 11:11:36 +0200

    fix(dev) remove environment variable

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -3,10 +3,7 @@ import { Server, Socket } from 'socket.io';
 import { ChatService } from './chat.service';
 import { forwardRef, Inject } from '@nestjs/common';
 
-@WebSocketGateway(
-  // tslint:disable-next-line: radix
-  parseInt(process.env.WSS_PORT) || 4783,
-)
+@WebSocketGateway()
 export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
   constructor(
