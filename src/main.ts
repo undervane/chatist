024a583fe8355024f2f8c83d732e20122c34ad9c
Undervane <sergio@mipigu.com>
2019-09-16 15:00:03 +0200

    refactor(dev) Change default startup port

--- src/main.ts
+++ src/main.ts
@@ -26,6 +26,6 @@ async function bootstrap() {
   }
 
   app.enableCors();
-  await app.listen(process.env.APP_PORT || 3000, process.env.APP_HOST || '0.0.0.0');
+  await app.listen(process.env.APP_PORT || 8080, process.env.APP_HOST || '0.0.0.0');
 }
 bootstrap();
